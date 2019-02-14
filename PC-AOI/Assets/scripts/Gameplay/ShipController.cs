7625e31a8697ddabb5cfcac14e09f3b7048e54b8
TronkethSivakethan Suddapalli <chinnanah@gmail.com>
2019-02-14 22:34:10 +0530

    added animations, made movement using acceleration

--- PC-AOI/Assets/scripts/Gameplay/ShipController.cs
+++ PC-AOI/Assets/scripts/Gameplay/ShipController.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    Rigidbody2D rgbd;
+    public Vector3 move;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rgbd = GetComponent<Rigidbody2D>();
+        move=new Vector3(0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal")*0.3f,Input.GetAxis("Vertical")*0.3f,0);
-        transform.position += move;
+        move.y = Input.GetAxis("Vertical") * 8f;
+        move.x = Input.GetAxis("Horizontal") * 8f;
+
+        animator.SetInteger("move",(int) move.x);
+
+        rgbd.AddForce(move*7,ForceMode2D.Force); //force movement option
+        //rgbd.velocity = move; //velocity manipulation option
+
+        //This part is for rotating the ship in the direction of movement
     }
 }
